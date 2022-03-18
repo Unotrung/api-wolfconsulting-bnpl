ca8122d9b2cb37f7e464b28bdd3031bc296c919f
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-03-18 14:47:04 +0700

    update api

--- routers/CommonRouter.js
+++ routers/CommonRouter.js
@@ -2,7 +2,7 @@ const CommonController = require('../controllers/CommonController');
 
 const router = require("express").Router();
 
-router.post("/generateContract", CommonController.generateContract);
+router.get("/generateContract", CommonController.generateContract);
 router.get("/generateProviders", CommonController.generateProviders);
 
 module.exports = router;
