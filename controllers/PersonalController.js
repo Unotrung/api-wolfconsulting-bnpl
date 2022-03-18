01b522cfb07093cd4e09a601f3bde7a97c2522d4
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-03-18 09:56:07 +0700

    Update Code

--- controllers/PersonalController.js
+++ controllers/PersonalController.js
@@ -1,5 +1,6 @@
 const Personal = require('../models/Personal');
 const User = require('../models/User');
+const { v4: uuid } = require('uuid');
 const logEvents = require('../helpers/logEvents');
 
 const PersonalController = {
