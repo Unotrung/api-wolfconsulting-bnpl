49716d079358d9d5991be0429b5b823540d59323
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-03-21 17:29:39 +0700

    Push API getHVToken

--- controllers/CommonController.js
+++ controllers/CommonController.js
@@ -1,3 +1,8 @@
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const CommonController = {
 
     generateContract: (req, res, next) => {
@@ -17,6 +22,40 @@ const CommonController = {
         return res.status(200).json({
             data: providers
         })
+    },
+
+    getHVToken: async (req, res, next) => {
+        try {
+            const url = "https://auth.hyperverge.co/login";
+            const options = {
+                method: "POST",
+                body: JSON.stringify({
+                    appId: process.env.appId,
+                    appKey: process.env.appKey,
+                    expiry: 900
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
+            const response = await fetch(url, options);
+            const data = await response.json();
+            if (data !== null) {
+                return res.json({
+                    token: data.result.token.split(" ")[1],
+                    status: true
+                })
+            }
+            else {
+                return res.json({
+                    message: "Fail To Get API",
+                    status: false,
+                })
+            }
+        }
+        catch (err) {
+            next(err);
+        }
     }
 
 };
--- routers/CommonRouter.js
+++ routers/CommonRouter.js
@@ -1,8 +1,10 @@
 const CommonController = require('../controllers/CommonController');
+const MiddlewareController = require('../controllers/MiddlewareController');
 
 const router = require("express").Router();
 
-router.get("/generateContract", CommonController.generateContract);
-router.get("/generateProviders", CommonController.generateProviders);
+router.get("/generateContract", MiddlewareController.verifyToken, CommonController.generateContract);
+router.get("/generateProviders", MiddlewareController.verifyToken, CommonController.generateProviders);
+router.get("/getHVToken", MiddlewareController.verifyToken, CommonController.getHVToken);
 
 module.exports = router;
