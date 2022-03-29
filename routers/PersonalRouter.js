afa96bb10de0a7488553d7d02d43e89fde13561c
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-03-29 16:46:04 +0700

    Update API getInfomation

--- routers/PersonalRouter.js
+++ routers/PersonalRouter.js
@@ -2,9 +2,9 @@ const PersonalController = require('../controllers/PersonalController');
 const MiddlewareController = require('../controllers/MiddlewareController');
 const router = require("express").Router();
 
+router.get("/:phone", MiddlewareController.VerifyTokenByMySelf, PersonalController.getInfomation);
 router.post("/addInfoPersonal", PersonalController.addInfoPersonal);
 router.get("/getAllBNPLInformation", PersonalController.getAllBNPLInformation);
-router.get("/:phone", MiddlewareController.VerifyTokenByMySelf, PersonalController.getInfomation);
 router.post("/registerProvider", PersonalController.registerProvider);
 router.delete("/deletePersonalandAccount", PersonalController.deletePersonalandAccount);
 
