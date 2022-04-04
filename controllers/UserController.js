c38820078a1ca51123627284341b9818d9d0feb9
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-04-04 22:18:57 +0700

    Update verifyOtp

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -71,7 +71,7 @@ const UserController = {
                         message: "This phone number is not exists !",
                         status: false,
                         errCode: 1003,
-                        step: 0
+                        step: 1
                     });
                 }
             }
@@ -208,7 +208,7 @@ const UserController = {
                         message: "Login successfully",
                         data: { ...others },
                         token: accessToken,
-                        status: true
+                        status: true,
                     });
                 }
             }
@@ -282,12 +282,12 @@ const UserController = {
                         await Otp.deleteMany({ phone: lastOtp.phone })
                             .then((data, err) => {
                                 if (!err) {
-                                    Customer.updateOne({ phone: phone }, { $set: { step: 3 } });
+                                    await Customer.updateOne({ phone: phone }, { $set: { step: 3 } });
+                                    return res.status(200).json({
+                                        message: "Successfully. OTP valid",
+                                        status: true,
+                                    })
                                 }
-                                return res.status(200).json({
-                                    message: "Successfully. OTP valid",
-                                    status: true,
-                                })
                             })
                     }
                     else {
--- models/bnpl_customers.js
+++ models/bnpl_customers.js
@@ -21,7 +21,7 @@ const bnpl_customerSchema = new mongoose.Schema({
         required: [true, 'Pin is required'],
     },
     step: {
-        type: String,
+        type: Number,
     }
 
 }, { timestamps: true });
