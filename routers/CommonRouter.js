9902bf504113a5ddf42b20da9342407e84ea7ecc
nguyenkien12345 <nguyenkien11202000@gmail.com>
2022-05-19 21:48:03 +0700

    Update All Router CommomRouter

--- routers/CommonRouter.js
+++ routers/CommonRouter.js
@@ -9,5 +9,9 @@ router.put("/updateStep",
         check('phone').matches(/^(09|03|07|08|05)+([0-9]{8}$)/).withMessage('Invalid phone number format'),
     ],
     MiddlewareController.validateRequestSchema, CommonController.updateStep);
+router.get("/getAllCity", CommonController.getAllCity);
+router.get("/getAllDistrict", CommonController.getAllDistrict);
+router.get("/getAllWard", CommonController.getAllWard);
+router.get("/getAllReferenceRelation", CommonController.getAllReferenceRelation);
 
 module.exports = router;
